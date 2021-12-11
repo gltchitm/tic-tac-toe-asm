857735f8105718c34bd632a59b4624f71dc885f0
gltchitm <42951031+gltchitm@users.noreply.github.com>
2021-12-11 13:50:50 -0500

    cleanup

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ cd $(dirname "$0")
 if [ ! -d build ]; then
     mkdir build
 fi
+
 nasm -felf64 src/main.asm -o build/tictactoe.o
 ld build/tictactoe.o -o build/tictactoe.out
 ./build/tictactoe.out
--- util/extract_game_data.py
+++ util/extract_game_data.py
@@ -4,9 +4,9 @@ def calculate_masks(*bases):
     masks = []
     for base in bases:
         masks.append({
-            "empty": base,
-            "x": base * 2,
-            "o": base * 4
+            'empty': base,
+            'x': base * 2,
+            'o': base * 4
         })
     return masks
 
@@ -15,22 +15,22 @@ masks = calculate_masks(1, 8, 64, 512, 4096, 32768, 262144, 2097152, 16777216)
 PLAYER_X_TURN = 134217728
 PLAYER_O_TURN = 1073741824
 
-if __name__ == "__main__":
-    r10 = int(input("Enter the game data bitmask: "))
+if __name__ == '__main__':
+    r10 = int(input('Enter the game data bitmask: '))
 
     for i in range(9):
-        if r10 & masks[i]["empty"]:
-            print(f"Spot {i} is: Empty")
-        elif r10 & masks[i]["x"]:
-            print(f"Spot {i} is: X")
-        elif r10 & masks[i]["o"]:
-            print(f"Spot {i} is: O")
+        if r10 & masks[i]['empty']:
+            print(f'Spot {i} is: Empty')
+        elif r10 & masks[i]['x']:
+            print(f'Spot {i} is: X')
+        elif r10 & masks[i]['o']:
+            print(f'Spot {i} is: O')
         else:
-            print(f"Could not extract data for spot {i}!")
+            print(f'Could not extract data for spot {i}!')
 
     if r10 & PLAYER_X_TURN:
-        print("Turn: X")
+        print('Turn: X')
     elif r10 & PLAYER_O_TURN:
-        print("Turn: O")
+        print('Turn: O')
     else:
-        print("COuld not extract turn!")
+        print('Could not extract turn!')
